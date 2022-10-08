ae8f5d56edf3f028c75cc64459c1595b5486a74e
AugustoBarcelos <augustoebo@taginfraestrutura.com.br>
2022-10-08 10:59:05 -0300

    feat: added second part of backend

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,17 @@ const app = express();
 const errorMiddleware = require('./middlewares/errors');
 
 app.use(express.json());
+
+
+
 //Import all routes
 const products = require('./routes/products');
+const auth = require('./routes/auth');
 
 //Middleware to handle errors
 app.use(errorMiddleware);
 
 app.use('/api/v1', products)
+app.use('/api/v1', auth)
+
 module.exports = app;
