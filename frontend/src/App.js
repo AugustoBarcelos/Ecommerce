3c95e2b1946a11c673de2262f33dbb5e7ce49e35
AugustoBarcelos <augustoebo@taginfraestrutura.com.br>
2022-10-26 20:39:32 -0300

    ft latest changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,22 @@ import './App.css';
 import Header from './components/layout/Headers';
 import Footer from './components/layout/Footer';
 import Home from './components/Home';
+import ProductDetails from './components/product/ProductDetails';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <div className="container container-fluid">
-          <Routes> 
-            <Route path="/" element={<Home />} />
-          </Routes>
+    <Routes>
+      <Router>
+        <div className="App">
+          <Header />
+          <div className="container container-fluid">
+            <Route path="/" component={Home} exact />
+            <Route path="/product/:id" component={ProductDetails} exact />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </Router>
+      </Router>
+    </Routes>
   );
 }
 
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,21 +4,29 @@ import MetaData from './layout/MetaData'
 import Product from './product/Product';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProducts } from '../actions/productActions';
-
+import Loader from './layout/Loader';
+import { useAlert } from 'react-alert';
 
 const Home = () => {
 
   const { loading, products, error, productsCount, resPerPage, filteredProductsCount } = useSelector(state => state.products)
 
+  const alert = useAlert();
   const dispatch = useDispatch();
 
   useEffect(() => {
+    if (error) {
+      alert.success('Success')
+      return alert.error(error)
+    }
     dispatch(getProducts());
-  }, [dispatch])
+
+
+  }, [dispatch, alert, error])
 
   return (
     <Fragment>
-      {loading ? <h1>Loading...</h1> : (
+      {loading ? <Loader /> : (
         <Fragment>
           <MetaData title={'TCC_AUGUSTO OLIVEIRA'} />
           <h1 id="products_heading">Latest Products</h1>
