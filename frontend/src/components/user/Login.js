237500a22de452ce1cf4230580175a1bea29160a
AugustoBarcelos <augustoebo@taginfraestrutura.com.br>
2022-11-06 11:36:25 -0300

    adjusted use-location

--- frontend/src/components/user/Login.js
+++ frontend/src/components/user/Login.js
@@ -1,5 +1,5 @@
-import React, { Fragment, useState, useLocation, useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React, { Fragment, useState, useEffect } from 'react'
+import { Link, useNavigate, useLocation } from 'react-router-dom'
 
 import Loader from '../layout/Loader'
 import MetaData from '../layout/MetaData'
